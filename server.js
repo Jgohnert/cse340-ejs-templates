1f06c1e9eec87e86bba8647b2fba43917eaf15c5
Jgohnert <goh22001@byui.edu>
2025-05-01 11:11:08 -0600

    fixed some of the syntax

--- server.js
+++ server.js
@@ -5,12 +5,12 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
+const express = require("express");
 // we tell the application to require express-ejs-layouts, so it can be used.
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
 
 /* ***********************
  * View Engine and Templates
@@ -18,19 +18,19 @@ const static = require("./routes/static")
 // we declare that ejs will be the view engine for our application. 
 // Built into EJS is the understanding that all views will be stored in a views 
 // folder, and that is where EJS will look for all view files.
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 // tells the application to use the express-ejs-layouts package
-app.use(expressLayouts)
+app.use(expressLayouts);
 // when the express ejs layout goes looking for the basic template for a view, 
 // it will find it in a layouts folder, and the template will be named layout.
 // EJS will read the path ./layouts/layout and look in the views folder 
 // (indicated by the period), then for a subfolder named layouts, then for a file named layout.ejs.
-app.set("layout", "./layouts/layout") // not at views root
+app.set("layout", "./layouts/layout"); // not at views root
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
 
 // Index route
 // app.get - The express application will watch the "get" object, within the HTTP Request, for a particular route.
@@ -39,18 +39,18 @@ app.use(static)
 // {title: "Home" } - The curly braces are an object (treated like a variable), which holds a name - value pair. This object supplies the value that the "head" partial file expects to receive. The object is passed to the view.
 app.get("/", function(req, res) {
   res.render("index", {title: "home"})
-})
+});
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
-})
+});
