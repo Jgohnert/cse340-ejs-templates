ad67131e3abfbc9af48a150ec12c3351745dc646
Jgohnert <goh22001@byui.edu>
2025-04-30 19:42:41 -0600

    added ejs template

--- routes/static.js
+++ routes/static.js
@@ -1,13 +1,21 @@
+// The express package is brought into the scope of the file and assigned to a local variable.
 const express = require('express');
+// The Express "router" functionality is invoked and stored into a local variable for use.
 const router = express.Router();
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
+
+// Indicates that the Express router is to "use" the "express.static" function, meaning this is where static resources will be found, with the public folder.
 router.use(express.static("public"));
+// Indicates that any route that contains /css is to refer to the public/css folder, which is found at the root level of the project.
 router.use("/css", express.static(__dirname + "public/css"));
+// Indicates that any route that contains /js is to refer to the public/js folder, which is found at the root level of the project.
 router.use("/js", express.static(__dirname + "public/js"));
+// Indicates that any route that contains /images is to refer to the public/images folder, which is found at the root level of the project.
 router.use("/images", express.static(__dirname + "public/images"));
 
+// exports the router object, along with all of these use statements for use in other areas of the application. This is VERY IMPORTANT. If a resource is NOT exported, it cannot be used somewhere else.
 module.exports = router;
 
 
--- server.js
+++ server.js
@@ -6,14 +6,40 @@
  * Require Statements
  *************************/
 const express = require("express")
+// we tell the application to require express-ejs-layouts, so it can be used.
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * View Engine and Templates
+ *************************/
+// we declare that ejs will be the view engine for our application. 
+// Built into EJS is the understanding that all views will be stored in a views 
+// folder, and that is where EJS will look for all view files.
+app.set("view engine", "ejs")
+// tells the application to use the express-ejs-layouts package
+app.use(expressLayouts)
+// when the express ejs layout goes looking for the basic template for a view, 
+// it will find it in a layouts folder, and the template will be named layout.
+// EJS will read the path ./layouts/layout and look in the views folder 
+// (indicated by the period), then for a subfolder named layouts, then for a file named layout.ejs.
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
+
+// Index route
+// app.get - The express application will watch the "get" object, within the HTTP Request, for a particular route.
+// "/" - This is route being watched. It indicates the base route of the application or the route which has no specific resource requested.
+// res.render() - The "res" is the response object, while "render()" is an Express function that will retrieve the specified view - "index" - to be sent back to the browser.
+// {title: "Home" } - The curly braces are an object (treated like a variable), which holds a name - value pair. This object supplies the value that the "head" partial file expects to receive. The object is passed to the view.
+app.get("/", function(req, res) {
+  res.render("index", {title: "home"})
+})
 
 /* ***********************
  * Local Server Information
